3149e27f07008775a6a1b70b897ffe01eded51b9
Umamaheswararao Meka <maheshmeka16@gmail.com>
2023-05-03 19:39:54 +0530

    Update mysql.sql

--- statefulsets/mysql/mysql.sql
+++ statefulsets/mysql/mysql.sql
@@ -1,4 +1,4 @@
 USE test;
-CREATE TABLE messages (ID INT identity(1,1), message VARCHAR(250))
+CREATE TABLE messages (ID INT NOT NULL AUTO_INCREMENT, message VARCHAR(250))
 INSERT INTO messages VALUES ('Hello')
 INSERT INTO messages VALUES ('Hey')
